e9aae3312cb97aef1ea36992ff292991b38fa656
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2021-12-23 16:01:56 +0000

    A bit of hacking at the packagify script -- definitely not ready for production.

--- packagify.sh
+++ packagify.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Exploit previous git install to bootstrap, even though we will reinstall it
-cd /
-git clone https://github.com/Cambridge-AdvancedOS/advopsys-packages.git
-cd advopsys-packages
-cd ..
+#cd /
+#git clone https://github.com/Cambridge-AdvancedOS/advopsys-packages.git
+#cd advopsys-packages
+#cd ..
 
 # Blow away the existing package system, which isn't properly registered
 rm -Rf /usr/local/*
@@ -15,21 +15,24 @@ pkg
 
 mkdir -p /data
 
-pkg add -y			\
-	bash			\
-	calc			\
-	git			\
-	sudo			\
-	tmux			\
-	py38-python-dtrace	\
-	py38-jupyterlab		\
-	py38-numpy		\
-	py38-pandas		\
-	py38-pygraphviz
+cd packages/All
+pkg add					\
+	bash-5.1.12.pkg			\
+	calc-2.14.0.12.pkg		\
+	git-2.34.1.pkg			\
+	sudo-1.9.8p2.pkg		\
+	tmux-3.2a.pkg			\
+	py38-jupyterlab-3.1.14.pkg	\
+	py38-numpy-1.20.3,1.pkg		\
+	py38-pandas-1.2.5,1.pkg		\
+	py38-python-dtrace-0.0.13.pkg	\
+	py38-pygraphviz-1.6.pkg
+
+cd ../..
 
 # Unclear why this is required, but it appears to be.
 # jupyter-lab build
 
-cd /root
-ssh-keygen -f /root/.ssh/id_rsa -N ''
-cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+#cd /root
+#ssh-keygen -f /root/.ssh/id_rsa -N ''
+#cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
